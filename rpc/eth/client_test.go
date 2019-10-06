e350ae5b85bc46e6f3aecd94a9fc437d23711ce1
yayanyang <org.lemon@gmail.com>
2019-10-06 12:15:25 +0800

    update

--- rpc/eth/client_test.go
+++ rpc/eth/client_test.go
@@ -5,13 +5,15 @@ import (
 	"fmt"
 	"testing"
 
+	"github.com/stretchr/testify/require"
+
 	"github.com/stretchr/testify/assert"
 )
 
 var client Client
 
 func init() {
-	client = New("https://ropsten.infura.io/OTFK50Z1PCljMOeEAlA9")
+	client = New("https://mainnet.infura.io/v3/44ab06a5fca644df953378ac1c16d2b9")
 }
 
 func TestGasPrice(t *testing.T) {
@@ -30,4 +32,10 @@ func TestHex(t *testing.T) {
 	b, err := hex.DecodeString("7472616e73666572")
 	assert.NoError(t, err)
 	println(string(b))
+}
+
+func TestBlance(t *testing.T) {
+	val, err := client.GetBalance("0x9f8f72aa9304c8b593d555f12ef6589cc3a579a2")
+	require.NoError(t, err)
+	println(val.String())
 }
